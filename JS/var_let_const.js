0808f41bf7c74b38a4a25ceb02e767bb7c39bb0f
Leyla Kandalova <kandallova@gmail.com>
2025-02-17 23:43:03 +0500

    basic js concepts

---
+++ JS/var_let_const.js
@@ -0,0 +1,8 @@
+let admin, name;
+name = 'John'
+admin=name
+console.log(admin)
+
+console.log('typeof null + 6: ', typeof null+6);
+console.log('typeof(null+6): ', typeof(null+6));
+console.log('typeof(NaN): ', typeof(NaN));
